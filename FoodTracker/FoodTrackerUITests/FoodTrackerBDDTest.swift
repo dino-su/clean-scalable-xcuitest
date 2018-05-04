8127129aea4e45a8054fc56f994cba80245f2633
Dino Su <chinglong.su@gmail.com>
2018-05-04 23:07:41 +0800

    manage steps by snippet.

--- FoodTracker/FoodTrackerUITests/FoodTrackerBDDTest.swift
+++ FoodTracker/FoodTrackerUITests/FoodTrackerBDDTest.swift
@@ -3,34 +3,32 @@ import XCTest
 class FoodTrackerBDDTest: XCUITestCase {
     var foodTracker: Page!
     
-    override func setUp() {
-        super.setUp()
-        
-        foodTracker = TestBuilder(app).setLocale().launch()
-    }
-    
     func testAddMeal() {
-        Given("I am on add meal screen") {
-            foodTracker.on(page: MainPage.self).openAdd()
+        Given("I am in Main screen") {
+            foodTracker = TestBuilder(app).setLocale().launch()
         }
         
         When("I add meal with name 'Kung Pao Chicken'") {
-            foodTracker.on(page: AddPage.self).addMeal(name: "Kung Pao Chicken")
+            foodTracker.on(page: MainPage.self).openAdd().addMeal(name: "Kung Pao Chicken")
         }
         
-        Then("I will see 'Kung Pao Chicken' appear on main screen") {
+        Then("I see 'Kung Pao Chicken' appear in Main screen") {
             foodTracker.on(page: MainPage.self).checkHasMeal(name: "Kung Pao Chicken")
         }
     }
     
     func testDeleteMeal() {
-        Given("I am on add meal screen") { }
+        Given("meal 'Kung Pao Chicken' exist") {
+            foodTracker = TestBuilder(app).setLocale().launch()
+
+            foodTracker.on(page: MainPage.self).checkHasMeal(name: "Kung Pao Chicken")
+        }
         
         When("I delete 'Kung Pao Chicken'") {
             foodTracker.on(page: MainPage.self).deleteMeal(name: "Kung Pao Chicken")
         }
         
-        Then("'Kung Pao Chicken' will dispear from main screen") {
+        Then("'Kung Pao Chicken' dispear from main screen") {
             foodTracker.on(page: MainPage.self).checkHasMeal(name: "Kung Pao Chicken", expected: false)
         }
     }
--- FoodTracker/FoodTrackerUITests/GherkinHelper.swift
+++ FoodTracker/FoodTrackerUITests/GherkinHelper.swift
@@ -1,36 +1,21 @@
 import XCTest
 
-/*
- * Credit: https://github.com/mvemjsun/Xcode83
- */
 extension XCTest {
     
     func Given(_ text: String, step: () -> Void ) {
-        XCTContext.runActivity(named: #function + " " + text) { _ in
+        XCTContext.runActivity(named: "Given " + text) { _ in
             step()
         }
     }
     
     func When(_ text: String, step: () -> Void ) {
-        XCTContext.runActivity(named: #function + " " + text) { _ in
+        XCTContext.runActivity(named: "When " + text) { _ in
             step()
         }
     }
     
     func Then(_ text: String, step: () -> Void ) {
-        XCTContext.runActivity(named: #function + " " + text) { _ in
-            step()
-        }
-    }
-    
-    func And(_ text: String, step: () -> Void ) {
-        XCTContext.runActivity(named: #function + " " + text) { _ in
-            step()
-        }
-    }
-    
-    func But(_ text: String, step: () -> Void ) {
-        XCTContext.runActivity(named: #function + " " + text) { _ in
+        XCTContext.runActivity(named: "Then " + text) { _ in
             step()
         }
     }
